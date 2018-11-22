1f74ce67672cab5c37994f346bb6c875a0092b97
root <root@ip-172-31-18-162.ap-southeast-1.compute.internal>
2018-11-22 08:04:57 +0000

    Second Commit

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 # Download the ghost:latest docker_image "image_id" to the system using a Terraform resource
-resource "docker_image" "image_id2" {
+resource "docker_image" "image_id" {
   name = "ghost:latest"
-}
-
-resource "docker_image" "image_id1" {
-  name = "nginx:latest"
 }
